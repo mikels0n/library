7db604dd08ecf09225c4f770b7fa6bd0a46854c8
mikels0n <myskamichal@gmail.com>
2024-07-17 16:24:38 +0200

    'read' value load fixed

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ addBook.addEventListener('click', () => {
     var title = document.getElementById('title').value;
     var author = document.getElementById('author').value;
     var pages = document.getElementById('pages').value;
-    var read = "finished";                                  //OPRAVIT 
+    var read = document.querySelector('input[name="read"]:checked').value;
     var book = new Book(title, author, pages, read);
     addBookToLibrary(book);
     console.log(myLibrary);
@@ -30,7 +30,7 @@ addBook.addEventListener('click', () => {
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'yes');
 const rur = new Book('R.U.R.', 'Karel Čapek', '102 pages', );
 
-addBookToLibrary(theHobbit);
+addBookToLibrary(theHobbit)
 addBookToLibrary(rur);
 
 console.log(myLibrary);
